c03c01e479bbf524516b6c0a6b325a68616e6078
Yujan3333 <yujanbasnet18@gmail.com>
2023-05-26 21:13:02 +0545

    AfterExam

--- News_Classification_Project_Flask/app.py
+++ News_Classification_Project_Flask/app.py
@@ -8,12 +8,18 @@ from wtforms.validators import InputRequired, Length, Email, ValidationError
 from flask_bcrypt import Bcrypt
 from model import Multinomial
 from preprocessor import preprocess_text, stop_words,punctuation_words
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import secrets
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+# secretkey for admin
+secret_key = secrets.token_hex(16)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -106,7 +112,6 @@ def register():
     return render_template('register.html', form=form)
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     
@@ -124,8 +129,6 @@ def login():
     return render_template('login.html', form=form)
 
 
-
-
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
@@ -133,15 +136,29 @@ def logout():
     return redirect(url_for('home'))
 
 
-
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html' , username=current_user.username)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+
+# For admin page 
+admin = Admin(app)
+class UserAdminView(ModelView):
+    column_list = ('username', 'email')
+    column_searchable_list = ('username', 'email')
+admin.add_view(UserAdminView(User, db.session))
+@app.route('/admin')
+@login_required
+def admin_panel():
+    return redirect(admin.url)
+
 
 # Load the pickled model
 with open('news_pred_vectorizer.pickle', 'rb') as handle:
@@ -150,10 +167,6 @@ with open('news_pred_vectorizer.pickle', 'rb') as handle:
 
 with open('news_pred_model.pickle','rb') as handle:
     Multinomial = pickle.load(handle)
-
-# @app.route('/home')
-# def index():
-#     return render_template('home.html')
 
 @app.route('/predict', methods=['GET','POST'])
 @login_required
